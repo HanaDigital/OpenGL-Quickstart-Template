beaae53df2aa2fd1a0fa79af19999c16ee372a48
Shehryar <shehryar.dev@gmail.com>
2021-12-15 09:34:49 +0400

    OpenGL 4.6 GLFW Fix

--- OpenGLTemplate/src/Main.cpp
+++ OpenGLTemplate/src/Main.cpp
@@ -9,8 +9,8 @@ int main()
 {
 	// Init OpenGL
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	// Uncomment on MacOS
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
